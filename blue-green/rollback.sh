82a109c71177176c5164513c3e32e6952254f78a
Dinesh katwal <dinesh.katwal@logicabeans.com>
2018-12-20 17:50:56 +0545

    Updated script

--- blue-green/rollback.sh
+++ blue-green/rollback.sh
@@ -3,5 +3,5 @@
 port1=$1
 port2=$2
 echo "Replacing $port1 with $port2"
-sed -i "s/$port1/$port2/g" ./haproxy/haproxy.cfg
-#chmod 777 ./haproxy/haproxy.cfg
+sed -i "s/$port1/$port2/g" haproxy.cfg
+docker exec -it $(docker ps -f name=loadbalancer -q) /bin/bash -c "chmod 755 /etc/haproxy/haproxy.cfg"
